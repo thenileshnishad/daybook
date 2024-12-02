7f2deb0e5a22f3410b687b5dd7c5058446754955
thenileshnishad <thenileshnishad@gmail.com>
2024-12-02 19:04:33 +0530

    Implement saving user data to MongoDB on signup

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,27 @@
+const User = require("../models/userModel");
+
 const signup = async (req, res) => {
   try {
     const { email, firstName, lastName, password } = req.body;
-    res.send(firstName);
+
+    const userExist = await User.findOne({ email });
+
+    if (userExist) {
+      return res.status(400).json({
+        message: "User already exist!",
+      });
+    }
+
+    const user = await User.create({ email, firstName, lastName, password });
+
+    res.status(201).json({
+      message: "User signed up successfully!",
+      user: {
+        email: user.email,
+        firstName: user.firstName,
+        lastName: user.lastName,
+      },
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({
