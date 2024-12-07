67a5868fc8c1cb04655efbd20dbce753addd5004
thenileshnishad <thenileshnishad@gmail.com>
2024-12-07 19:32:24 +0530

    Implement user profile update functionality

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,6 @@
-const me = (req, res) => {
+const User = require("../models/userModel");
+
+const viewProfile = (req, res) => {
   const { email, firstName, lastName } = req.user;
   res.status(200).json({
     message: "Profile fetch successfully!",
@@ -6,4 +8,42 @@ const me = (req, res) => {
   });
 };
 
-module.exports = { me };
+const updateProfile = async (req, res) => {
+  const loggedUser = req.user;
+  const { firstName, lastName } = req.body;
+
+  if (!firstName || !lastName) {
+    return res.status(400).json({
+      message: "All fields are required!",
+    });
+  }
+
+  if (firstName.length > 50 || lastName.length > 50) {
+    return res.status(400).json({
+      message: "First Name and Last Name length should be less than 50!",
+    });
+  }
+
+  try {
+    const updateUser = await User.findByIdAndUpdate(
+      loggedUser._id,
+      {
+        firstName,
+        lastName,
+      },
+      { new: true }
+    );
+
+    res.status(200).json({
+      message: "Profile updated successfully!",
+      data: { firstName: updateUser.firstName, lastName: updateUser.lastName },
+    });
+  } catch (error) {
+    console.error("Error updating profile!: ", error);
+    res.status(500).json({
+      message: "Something went wrong! Please try again later!",
+    });
+  }
+};
+
+module.exports = { viewProfile, updateProfile };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const authMiddleware = require("../middleware/authMiddleware");
 const userController = require("../controllers/userController");
 
-router.get("/me", authMiddleware, userController.me);
+router.get("/me", authMiddleware, userController.viewProfile);
+router.put("/me", authMiddleware, userController.updateProfile);
 
 module.exports = router;
