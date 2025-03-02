ce897c49f944ba3e5491fec4d407f2108e0ddcc1
Nilesh Kumar Nishad <thenileshnishad@gmail.com>
2025-03-02 23:25:01 +0530

    Implement basic login and signup pages

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,6 +1,91 @@
+import { useState } from "react";
+
 const Signup = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => {
+      return { ...prevData, [name]: value };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>Signup</div>
-  )
-}
-export default Signup
+    <div className="flex justify-center items-center min-h-[calc(100vh-64px-52px)]">
+      <div className="card bg-base-300 w-full max-w-sm shrink-0 shadow-2xl">
+        <div className="card-body">
+          <h2 className="card-title block text-center">Sign up to DayBook</h2>
+          <form onSubmit={handleSubmit}>
+            <fieldset className="fieldset w-xs p-2 pl-6">
+              <label htmlFor="firstname" className="fieldset-label mb-1">
+                Name
+              </label>
+              <input
+                id="firstname"
+                type="text"
+                className="input mb-2"
+                placeholder="Enter name"
+                onChange={handleChange}
+                name="firstName"
+                value={formData.firstName}
+              />
+
+              <label htmlFor="lastname" className="fieldset-label mb-1">
+                Last Name
+              </label>
+              <input
+                id="lastname"
+                type="text"
+                className="input mb-2"
+                placeholder="Enter last name"
+                onChange={handleChange}
+                name="lastName"
+                value={formData.lastName}
+              />
+
+              <label htmlFor="email" className="fieldset-label mb-1">
+                Email
+              </label>
+              <input
+                id="email"
+                type="email"
+                className="input mb-2"
+                placeholder="Enter email"
+                onChange={handleChange}
+                name="email"
+                value={formData.email}
+              />
+
+              <label htmlFor="password" className="fieldset-label mb-1">
+                Password
+              </label>
+              <input
+                id="password"
+                type="password"
+                className="input"
+                placeholder="Password"
+                onChange={handleChange}
+                name="password"
+                value={formData.password}
+              />
+
+              <button type="submit" className="btn btn-neutral mt-4 mb-2">
+                Sign up
+              </button>
+            </fieldset>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Signup;
