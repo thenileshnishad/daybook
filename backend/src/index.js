49cb7cb0a915faf5c0dd2c060082ec3a67106f08
Nilesh Kumar Nishad <thenileshnishad@gmail.com>
2025-03-04 21:25:30 +0530

    Add CORS origin support in backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,9 +3,11 @@ const app = express();
 require("dotenv").config();
 const connectDB = require("./config/database");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -5,6 +5,9 @@ const generateToken = (_id, res) => {
 
   res.cookie("token", token, {
     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+    // secure: true,
+    sameSite: "Strict",
   });
 };
 
