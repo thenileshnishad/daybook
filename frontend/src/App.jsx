4de31abf4c963a87254e9714f96ce150a75d59c4
Nilesh Kumar Nishad <thenileshnishad@gmail.com>
2025-03-04 23:57:55 +0530

    Setup RTK Query, store & add .env.example

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
@@ -10,17 +12,19 @@ import About from "./pages/About";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<Signup />} />
-          <Route path="entries" element={<Entries />} />
-          <Route path="about" element={<About />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="entries" element={<Entries />} />
+            <Route path="about" element={<About />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 };
 export default App;
---
+++ frontend/src/redux/api/apiSlice.js
@@ -0,0 +1,15 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const apiSlice = createApi({
+  reducerPath: "apiSlice",
+
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${import.meta.env.VITE_BACKEND_URL}/api`,
+    credentials: "include",
+  }),
+
+  endpoints: (builder) => ({}),
+});
+
+export const {} = apiSlice;
+export default apiSlice;
