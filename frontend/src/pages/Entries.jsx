3b21fa856b027f3f8610c17fb4d40cc0f826dac1
Nilesh Kumar Nishad <thenileshnishad@gmail.com>
2025-03-09 05:59:41 +0530

    Fetch all entries and display them using Entry Card

--- frontend/src/pages/Entries.jsx
+++ frontend/src/pages/Entries.jsx
@@ -1,6 +1,29 @@
+import { useGetEntriesQuery } from "../redux/api/entriesApiSlice";
+import EntryCard from "../components/entries/EntryCard";
+
 const Entries = () => {
+  const { data: getEntries, isLoading } = useGetEntriesQuery();
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  if (getEntries.data <= 0) {
+    return <p>No entries found!</p>;
+  }
+
   return (
-    <div>Entries</div>
-  )
-}
-export default Entries
+    <div className="flex flex-wrap gap-5 justify-center my-10 min-h-[calc(100vh-64px-52px)]">
+      {getEntries.data.map((entry) => (
+        <EntryCard
+          key={entry._id}
+          date={entry.date}
+          title={entry.title}
+          mood={entry.mood}
+          content={entry.content}
+          updatedAt={entry.updatedAt}
+        />
+      ))}
+    </div>
+  );
+};
+export default Entries;
